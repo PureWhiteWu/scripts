5ea99137d3135e9351d84649e5274eb05591327b
Pure White <wudi.daniel@bytedance.com>
2019-04-10 19:29:11 +0800

    add ss

---
+++ install-shadowsocks.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget --no-check-certificate -O ~/shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x ~/shadowsocks-all.sh
+~/shadowsocks-all.sh 2>&1 | tee ~/shadowsocks-all.log
