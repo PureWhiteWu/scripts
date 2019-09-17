079dae03acee872ea19318218bb47a9afd0f4871
Pure White <wudi.daniel@bytedance.com>
2019-09-18 02:02:38 +0800

    fix sudo

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,7 @@ apt update
 
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
