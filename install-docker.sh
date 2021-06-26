e257fe04772977435e61d786bc51fcfe47945471
Pure White <wudi.daniel@bytedance.com>
2021-06-26 13:55:04 +0800

    update

--- install-docker.sh
+++ install-docker.sh
@@ -4,14 +4,13 @@ apt remove -y docker docker-engine docker.io containerd runc
 
 apt update
 
-apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-add-apt-repository -y \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
 
---
+++ install-shadowsocks-rust.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+shadowsocks_rust_init='/etc/init.d/shadowsocks-rust'
+shadowsocks_rust_config='/etc/shadowsocks-rust/config.json'
+
+if [ ! -d "$(dirname ${shadowsocks_rust_config})" ]; then
+    mkdir -p $(dirname ${shadowsocks_rust_config})
+fi
+
+cp shadowsocks-rust-config ${shadowsocks_rust_config}
+
+apt install -y gcc g++
+
+git clone https://github.com/shadowsocks/shadowsocks-rust.git
+
+cd shadowsocks-rust
+
+git checkout v1.11.1
+
+rustup default nightly
+
+export RUSTFLAGS="-C target-cpu=native"
+
+cargo build --release
+
+cp target/release/ssserver /usr/local/bin
+
+cat > ${shadowsocks_rust_config}<<-EOF
+{
+    "server":"::",
+    "server_port":,
+    "password":"",
+    "timeout":300,
+    "method":"",
+    "keep_alive": 15,
+    "mode":"tcp_and_udp"
+}
+EOF
+
+chmod +x ${shadowsocks_rust_init}
+service_name=$(basename ${shadowsocks_rust_init})
+update-rc.d -f "${service_name}" defaults
