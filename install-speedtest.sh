4258b4562b0619872606cf5836c67b30ea4c1a13
Pure White <wudi.daniel@bytedance.com>
2024-01-28 01:11:40 +0800

    update

--- install-speedtest.sh
+++ install-speedtest.sh
@@ -4,8 +4,6 @@ git clone https://github.com/adolfintel/speedtest.git ~/speedtest
 
 cd ~/speedtest
 
-git checkout docker
-
 docker build -t adolfintel/speedtest:latest .
 
 docker run -d --name speedtest --restart always -p 0.0.0.0:48888:80 adolfintel/speedtest:latest
